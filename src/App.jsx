110651edc780f2d1d574a64044cecf0d91135003
keerWake <keer8k@gmail.com>
2025-06-20 15:55:00 -0400

    Button feature
    Background changed to white, one-button feature was integrated w CQ colors. Attempted gmaps background (did not work :(

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import CourtsList from "./components/CourtsList";
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+//import './App.css'
 
 
 const API_KEY = import.meta.env.GOOGLE_MAPS_API_KEY
+
+const containerStyle = {
+  width: '100vh',
+  height: '100vh',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  zIndex: -1,
+};
 
 function App() {
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
   const [userLocation, setUserLocation] = useState(null)
+  const [screen, setScreen] = useState("splash");
 
 
 useEffect(() => {
   // Toggle between geolocation and custom coordinates
+  // Getting data from backend basically KK
 
   // Option 1: Use geolocation (default)
-  const useGeolocation = false;
+  const useGeolocation = true;
 
   if (useGeolocation && navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
@@ -34,6 +47,11 @@ useEffect(() => {
 }, []);
 
   useEffect(() => {
+    // if(!loading){
+    //   // make sure the splashscreen doesn't show when done loading
+    //   const timeout = setTimeout(() => setScreen("main"),1000);
+    //   return clearTimeout(timeout);
+    // }
     if (!userLocation) return;
 
     // Fetch court data with lat/lng
@@ -69,10 +87,33 @@ useEffect(() => {
   
   return (
     <div>
-      <h1>Tennis Court Availability</h1>
-      {data && <CourtsList courts={data} />}
+      {/* {data && <CourtsList courts={data} />} */}
+      <button>Start Your Quest</button>
     </div>
+
+
   )
 }
+
+function SplashScreen(){
+  const center = {
+    lat: 40.712,
+    lng: -74.006
+  };
+
+  return(
+    <div style={{position: "relative", height: "100vh"}}>
+    <LoadScript googleMapsApiKey={API_KEY}>
+      <GoogleMap
+      // mapContainerClassName={containerStyle}
+      center={center}
+      zoom={12}
+      >
+      </GoogleMap>  
+    </LoadScript>
+
+    </div>
+  )
+};
 
 export default App
